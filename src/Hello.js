a5fbfebf0029a333df67e3892b924e2596ea46ef
gulshanyadav01 <gulshany01@gmail.com>
2021-01-19 22:09:35 +0530

    yet to come

--- src/Hello.js
+++ src/Hello.js
@@ -22,13 +22,12 @@ const Hello = () => {
   }
     return(
         <div className = "w-full mt-52  ml-48   h-auto">
-          <div className = "w-48 h-48 rounded shadow-2xl bg-gulshan-dascar">
-            <form>
-              <input type = "file" className = "ml-10 mt-8" />
-              
-              <input type = "submit" value = "upload" className = "px-2 py-1 rounded cursor-pointer mt-8 ml-16 bg-orange-700 font-bold text-white"/>
-            </form>
-          </div>
+            <div className = "w-48 h-48 rounded shadow-2xl bg-gulshan-dascar">
+                <form>
+                    <input type = "file" className = "ml-10 mt-8" />
+                    <input type = "submit" value = "upload" className = "px-2 py-1 rounded cursor-pointer mt-8 ml-16 bg-orange-700 font-bold text-white"/>
+                </form>
+            </div>
         </div>
     )
 }
