a6a3042b8f098684b327f7125190f281a7f37b11
gulshanyadav01 <gulshany01@gmail.com>
2021-01-19 21:51:14 +0530

    yet to come

--- src/Hello.js
+++ src/Hello.js
@@ -24,15 +24,15 @@ const Hello = () => {
 
   }
     return(
-        <div className = "w-full mt-52  ml-48   h-auto">
-          <div className = "w-48 h-48 rounded shadow-2xl bg-gulshan-dascar">
-            <form onSubmit  = {onSubmit}>
-              <input type = "file" className = "ml-10 mt-8" />
-              {/* <label>for </label> */}
-              <input type = "submit" value = "upload" className = "px-2 py-1 rounded cursor-pointer mt-8 ml-16 bg-orange-700 font-bold text-white"/>
-            </form>
-          </div>
-        </div>
+            <div className = "w-full mt-52  ml-48   h-auto">
+                <div className = "w-48 h-48 rounded shadow-2xl bg-gulshan-dascar">
+                    <form onSubmit  = {onSubmit}>
+                        <input type = "file" className = "ml-10 mt-8" />
+                        
+                        <input type = "submit" value = "upload" className = "px-2 py-1 rounded cursor-pointer mt-8 ml-16 bg-orange-700 font-bold text-white"/>
+                    </form>
+                </div>
+            </div>
     )
 }
 export default Hello
