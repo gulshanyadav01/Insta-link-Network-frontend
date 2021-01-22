2ca3e1203c092ab1a634ec4fe2fe1be095dd1a1b
gulshanyadav01 <gulshany01@gmail.com>
2021-01-22 12:52:37 +0530

    file upload is not done yet

---
+++ src/components/posts/FileUpload.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FileUpload = () => {
+    return (
+        <div className = "relative  bg-blue-100">
+            <form>
+                <input type = "file"/>
+
+            </form>
+        </div>
+    )
+}
+
+export default FileUpload
