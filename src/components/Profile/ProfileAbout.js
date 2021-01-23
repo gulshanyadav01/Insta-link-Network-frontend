0ac78eb8e77fdf4fc59cd58373361f1d96c6d5ea
gulshanyadav01 <gulshany01@gmail.com>
2021-01-23 19:55:40 +0530

    yet to come

--- src/components/Profile/ProfileAbout.js
+++ src/components/Profile/ProfileAbout.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 const ProfileAbout = ({profile:{bio, skills, user:{name}}}) => {
     return (
         <div className = "w-full h-32 mt-4 rounded shadow bg-white">
-          <div>
-              <h1 className = "ml-8 font-bold  capitalize mt-4">{name}s  Bio </h1>
-              <h1 className = "ml-8">{bio}</h1>
-          </div>
+            <div>
+                <h1 className = "ml-8 font-bold  capitalize mt-4">{name}s  Bio </h1>
+                <h1 className = "ml-8">{bio}</h1>
+            </div>
         </div>
     )
 }
